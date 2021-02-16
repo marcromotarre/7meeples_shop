4c71dfd70da0c6c1e601fa2b3d3ed43d78dceac0
Marc Romo <marc.romo@crealogix.com>
2021-02-16 15:38:29 +0100

    try

--- pages/hasura/index.js
+++ pages/hasura/index.js
@@ -4,12 +4,18 @@ import { jsx } from "theme-ui";
 import { useRouter } from "next/router";
 import { Provider } from "react-redux";
 import { useEffect } from "react";
+import { create_account } from "src/backend/credentials";
 
 export default function Welcome() {
   const router = useRouter();
   useEffect(() => {
     console.log("read");
+    ddbb();
   }, []);
+
+  const ddbb = async () => {
+    create_account({ email: "marc romo", password: "blabla", code: "jijijij" });
+  };
 
   return <div>hasura</div>;
 }
