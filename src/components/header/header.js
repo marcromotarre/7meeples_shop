6184c83b8ccca707491c183c9ba5cf47fdd7304a
Marc Romo <marc.romo@crealogix.com>
2021-02-19 19:24:55 +0100

    v03

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -188,7 +188,7 @@ export default function Header() {
           <MenuItem className={menuState} text={"Button"}></MenuItem>
           <MenuItem className={menuState} text={"Button"}></MenuItem>
           <MenuItem className={menuState} text={"Button"}></MenuItem>
-          <MenuItem className={menuState} text={"v1.02"}></MenuItem>
+          <MenuItem className={menuState} text={"v1.03"}></MenuItem>
         </div>
       </div>
       <div
